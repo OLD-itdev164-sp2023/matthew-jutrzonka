9baa6276bdd56f9f22ace9bc3404ef835f422407
Matthew Jutrzonka <jutrzont@gmatc.matc.edu>
2023-04-07 17:14:01 -0500

    Create IconButton Component

--- blog/src/Button/IconButton.js
+++ blog/src/Button/IconButton.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { BaseButton } from "./BaseButton";
+import { BaseButton } from './BaseButton';
 
 const StyledButton = styled(BaseButton)`
     // Inject theme styles here later
